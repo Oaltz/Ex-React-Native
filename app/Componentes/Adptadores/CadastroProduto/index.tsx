99156a8e46fb66b2c3533f678012c3ae6b939182
Henrique <114785029+Oaltz@users.noreply.github.com>
2024-09-06 23:39:55 +0000

    aula06-09

--- app/Componentes/Adptadores/CadastroProduto/index.tsx
+++ app/Componentes/Adptadores/CadastroProduto/index.tsx
@@ -1,18 +1,38 @@
-import { useState } from "react"
-import { TextInput, View, Text } from "react-native"
 
-const CadastroProduto = ()=>{
-    let[nome,setNome] = useState('')
-    let[descricao,setDescricao] = useState('')
-    
+import { useState } from "react";
+import { TextInput } from "react-native-gesture-handler";
+import axios from "axios";
+import Produto from "@/app/Models/Produto";
 
+const CadastroProduto = () => {
+    let [nome, setNome] = useState('');
+    let [descricao, setDescricao] = useState('');
+    let [preco, setPreco] = useState(0);
 
     return(
-        <View>
-            <Text >Nome:</Text>
-            <TextInput onChangeText={setNome} value={nome}></TextInput>
-            <TextInput onChangeText={setDescricao} value={descricao}></TextInput>
-            
-        </View>
+        <view>
+          <text>Nome:</text>
+          <TextInput onChangeText={setNome} value= {nome} />
+          <text>Descrição:</text>
+          <TextInput onChangeText={setDescricao} value= {descricao} />
+          <text>Preço:</text>
+          <TextInput onChangeText={(v)=> 
+                            {setPreco(
+                                isNaN(parseFloat(v))? 0: parseFloat(v));} }
+          inputMode="numeric" value={preco.toString()}></TextInput>
+          </view>
+
     )
+
+    function Cadastrar(){
+        let produto: Produto = {
+          nome: nome,
+          descricao: descricao,
+          preco: preco,
+        } as Produto;
+        let api = 'https://api-docker-2t8m.onrender.com/api/produtos';
+        axios.post(api, produto)
+      }
+    
 }
+ export default CadastroProduto;
--- app/Componentes/Adptadores/ItemProduto/index.tsx
+++ app/Componentes/Adptadores/ItemProduto/index.tsx
@@ -1,7 +1,8 @@
 import Produto from "@/app/Models/Produto";
 import Style from "@/app/Styles/Default";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Button, Image, Text, View } from "react-native";
+import { ExclusiveGesture } from "react-native-gesture-handler/lib/typescript/handlers/gestures/gestureComposition";
 
 interface PropProd{
     produto:Produto,
@@ -10,11 +11,17 @@ interface PropProd{
 const ItemProduto: React.FC<PropProd> = ({produto})=>{
 
     console.log(produto)
+    function Excluir(id: number) {
+        throw new Error("Function not implemented.");
+    }
+
 return(
     <View style = {Style.card}>
         <Text style ={Style.cardText}>{produto.nome}</Text>
         <Text style = {Style.cardText}>{produto.preco}</Text>
-        <Image source = {{uri:produto.foto}} style={Style.image}></Image>
+        <Image source={{uri:produto.foto} } style={Style.image} ></Image>
+        <Button title="Excluir"
+            onPress={()=>{Excluir(produto.id)}}/>
     </View>
 )
 
